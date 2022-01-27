644a774bc70e2e53c1994743a32beebe67420df7
kojix2 <2xijok@gmail.com>
2022-01-27 10:38:28 +0900

    Use symbol

--- ext/umap/umap.cpp
+++ ext/umap/umap.cpp
@@ -23,23 +23,23 @@ Object umap_run(Object self, int ndim, int nthreads, int tick = 0)
   return self;
 }
 
-Object define_defaults(Object self)
+Hash define_defaults(Object self)
 {
   Hash d;
-  d[String("local_connectivity")]   = Umap::Defaults::local_connectivity;
-  d[String("bandwidth")]            = Umap::Defaults::bandwidth;
-  d[String("mix_ratio")]            = Umap::Defaults::mix_ratio;
-  d[String("spread")]               = Umap::Defaults::spread;
-  d[String("min_dist")]             = Umap::Defaults::min_dist;
-  d[String("a")]                    = Umap::Defaults::a;
-  d[String("b")]                    = Umap::Defaults::b;
-  d[String("repulsion_strength")]   = Umap::Defaults::repulsion_strength;
-  d[String("num_epochs")]           = Umap::Defaults::num_epochs;
-  d[String("learning_rate")]        = Umap::Defaults::learning_rate;
-  d[String("negative_sample_rate")] = Umap::Defaults::negative_sample_rate;
-  d[String("num_neighbors")]        = Umap::Defaults::num_neighbors;
-  d[String("seed")]                 = Umap::Defaults::seed;
-  d[String("batch")]                = Umap::Defaults::batch;
+  d[Symbol("local_connectivity")] = Umap::Defaults::local_connectivity;
+  d[Symbol("bandwidth")] = Umap::Defaults::bandwidth;
+  d[Symbol("mix_ratio")] = Umap::Defaults::mix_ratio;
+  d[Symbol("spread")] = Umap::Defaults::spread;
+  d[Symbol("min_dist")] = Umap::Defaults::min_dist;
+  d[Symbol("a")] = Umap::Defaults::a;
+  d[Symbol("b")] = Umap::Defaults::b;
+  d[Symbol("repulsion_strength")] = Umap::Defaults::repulsion_strength;
+  d[Symbol("num_epochs")] = Umap::Defaults::num_epochs;
+  d[Symbol("learning_rate")] = Umap::Defaults::learning_rate;
+  d[Symbol("negative_sample_rate")] = Umap::Defaults::negative_sample_rate;
+  d[Symbol("num_neighbors")] = Umap::Defaults::num_neighbors;
+  d[Symbol("seed")] = Umap::Defaults::seed;
+  d[Symbol("batch")] = Umap::Defaults::batch;
 
   return d;
 }
--- lib/umap.rb
+++ lib/umap.rb
@@ -6,4 +6,11 @@ require_relative "umap/umap"
 module Umap
   class Error < StandardError; end
   # Your code goes here...
+
+  class << self
+    def umap(_y, method: "Annoy", ndim: 2, nthreads: 1, tick: 0, **options)
+      options.transform_keys!(&:to_sym)
+      args = define_defaults
+    end
+  end
 end
