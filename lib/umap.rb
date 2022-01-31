1f1d06a9b6f2298b2414bf7af9b0f6fce7438a28
kojix2 <2xijok@gmail.com>
2022-01-31 16:49:14 +0900

    Raise error when params are unknown

--- lib/umap.rb
+++ lib/umap.rb
@@ -12,7 +12,12 @@ module Umap
   class << self
     def umap(ary, method: :annoy, ndim: 2, nthreads: 1, tick: 0, **opts)
       opts.transform_keys!(&:to_sym)
-      params   = define_defaults.merge(opts)
+      params = default_parameters
+      unless (u = (opts.keys - params.keys)).empty?
+        raise ArgumentError, "[umappp.rb] unknown option : #{u.inspect}"
+      end
+
+      params.merge!(opts)
       nnmethod = %i[annoy vptree].index(method.to_sym)
       data     = Numo::SFloat.cast(ary).transpose
 
