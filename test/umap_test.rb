cc3781f8f7170ec627882b73069adf2563db12c5
kojix2 <2xijok@gmail.com>
2022-10-08 23:21:58 +0900

    Improved test little

--- test/umap_test.rb
+++ test/umap_test.rb
@@ -4,8 +4,19 @@ require "test_helper"
 
 class UmapppTest < Test::Unit::TestCase
   test "VERSION" do
-    assert do
-      ::Umappp.const_defined?(:VERSION)
+    assert Umappp.const_defined?(:VERSION)
+  end
+
+  test "default_parameters" do
+    assert_instance_of(Hash, Umappp.default_parameters)
+  end
+
+  test "run" do
+    r = nil
+    assert_nothing_raised do
+      r = Umappp.run(Numo::SFloat.new(10, 10).rand)
     end
+    assert_instance_of Numo::SFloat, r
+    assert_equal [10, 2], r.shape
   end
 end
