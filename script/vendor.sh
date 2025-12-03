057908f1c6bf0503fcfc6a3f0892b4e1f0779184
kojix2 <2xijok@gmail.com>
2025-12-03 21:38:49 +0900

    Update vendor.sh

--- script/vendor.sh
+++ script/vendor.sh
@@ -1,19 +1,28 @@
+#!/bin/bash
 # This clones the umappp repository and obtains its dependencies.
 # We vendor everything to avoid problems with managing compile-
 # time dependencies in parallel with the R package ecosystem.
 
+# Usage: UMAPPP_VERSION=v3.2.0 vendor.sh
+
 set -e
 set -u
+
+UMAPPP_VERSION=${UMAPPP_VERSION:-master}
 
 # Pulling down the dependencies.
 if [ ! -e temp ]
 then
-    git clone https://github.com/LTLA/umappp temp
-    cd temp 
+    git clone https://github.com/libscran/umappp temp
+    cd temp
+    git checkout ${UMAPPP_VERSION}
 else
     cd temp
-    git checkout master
-    git pull
+    git fetch
+    git checkout ${UMAPPP_VERSION}
+    if [ "${UMAPPP_VERSION}" = "master" ]; then
+        git pull
+    fi
 fi
 cmake -S . -B build -DBUILD_TESTING=OFF
 
